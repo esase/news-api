ba1e35a345c0d4470c1ee59e34b1086c0efca91f
Alex Ermashev <alex.ermashev@carrentalgateway.com>
2022-12-16 16:48:09 +0600

    Initial

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+import pkg from '../package.json';
+
+export default {
+    serviceName: pkg.name,
+    mongoHost: process.env.MONGO_HOST ?? ''
+};
---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import fastify from 'fastify';
+import { MongoClient, ObjectId } from 'mongodb';
+import config from './config';
+
+const mongoClient = new MongoClient(config.mongoHost);
+const newsCollection = mongoClient.db().collection('news');
+
+const server = fastify();
+
+interface News {
+    _id: string;
+    title: string;
+    body: string;
+}
+
+server.get('/', async () =>  newsCollection.find({}).toArray());
+
+server.post<{ Body: News, Reply: News }>('/', async (request) => {
+    const result = await newsCollection.insertOne({
+        title: request.body.title,
+        body: request.body.body
+    });
+
+    return await newsCollection.findOne({
+        _id: result.insertedId
+    }) as unknown as News;
+});
+
+server.put<{ Params: { id: string }, Body: News, Reply: News }>('/:id', async (request) => {
+    await newsCollection.updateOne({
+        _id: new ObjectId(request.params.id).valueOf()
+    }, {
+        $set: {
+            title: request.body.title,
+            body: request.body.body
+        }
+    });
+
+    return await newsCollection.findOne({
+        _id: new ObjectId(request.params.id).valueOf()
+    }) as unknown as News;
+});
+
+server.delete('/', async () => {
+    await newsCollection.deleteMany({});
+
+    return {};
+});
+
+server.delete<{ Params: { id: string } }>('/:id', async (request) => {
+    await newsCollection.deleteOne({
+        _id: new ObjectId(request.params.id).valueOf()
+    });
+
+    return {};
+});
+
+server.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
