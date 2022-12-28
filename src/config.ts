6f84b1d39d9f0d7607278a19ca7aceb7eb58ae56
Alex Ermashev <alex.ermashev@carrentalgateway.com>
2022-12-28 16:43:49 +0600

    add rabbitmq events

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,7 @@ import pkg from '../package.json';
 
 export default {
     serviceName: pkg.name,
-    mongoHost: process.env.MONGO_HOST ?? ''
+    mongoHost: process.env.MONGO_HOST ?? '',
+    rabbitUrl: process.env.RABBITMQ_URL ?? 'amqp://localhost'
 };
+
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,7 @@ import fastify from 'fastify';
 import { MongoClient, ObjectId } from 'mongodb';
 import config from './config';
 import client from 'prom-client';
-
-const mongoClient = new MongoClient(config.mongoHost);
-const newsCollection = mongoClient.db().collection('news');
-
-const server = fastify();
+import { connect } from 'amqplib';
 
 interface News {
     _id: string;
@@ -14,68 +10,86 @@ interface News {
     body: string;
 }
 
-const register = new client.Registry();
+const app = async () => {
+    const mongoClient = new MongoClient(config.mongoHost);
+    const newsCollection = mongoClient.db().collection('news');
 
-client.collectDefaultMetrics({
-    prefix: 'node_',
-    gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5],
-    register,
-    labels: { NODE_APP_INSTANCE: config.serviceName }
-});
+    const rabbitMqConnection = await connect(config.rabbitUrl);
+    const rabbitMq = await rabbitMqConnection.createChannel();
 
-server.get('/metrics', async (request, result) =>  {
-    result.header('Content-Type', register.contentType);
-    result.send(await register.metrics());
-});
+    const server = fastify();
 
-server.get('/ping', async () =>  'pong');
+    const register = new client.Registry();
 
-server.get('/', async () =>  newsCollection.find({}).toArray());
-
-server.post<{ Body: News, Reply: News }>('/', async (request) => {
-    const result = await newsCollection.insertOne({
-        title: request.body.title,
-        body: request.body.body
+    client.collectDefaultMetrics({
+        prefix: 'node_',
+        gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5],
+        register,
+        labels: { NODE_APP_INSTANCE: config.serviceName }
     });
 
-    return await newsCollection.findOne({
-        _id: result.insertedId
-    }) as unknown as News;
-});
+    server.get('/metrics', async (request, result) =>  {
+        result.header('Content-Type', register.contentType);
+        result.send(await register.metrics());
+    });
 
-server.put<{ Params: { id: string }, Body: News, Reply: News }>('/:id', async (request) => {
-    await newsCollection.updateOne({
-        _id: new ObjectId(request.params.id).valueOf()
-    }, {
-        $set: {
+    server.get('/ping', async () =>  'pong');
+
+    server.get('/', async () =>  newsCollection.find({}).toArray());
+
+    server.post<{ Body: News, Reply: News }>('/', async (request) => {
+        const result = await newsCollection.insertOne({
             title: request.body.title,
             body: request.body.body
+        });
+
+        rabbitMq.publish('amq.topic', 'news-api.news.created', Buffer.from(JSON.stringify({
+            id: result.insertedId
+        })));
+
+        return await newsCollection.findOne({
+            _id: result.insertedId
+        }) as unknown as News;
+    });
+
+    server.put<{ Params: { id: string }, Body: News, Reply: News }>('/:id', async (request) => {
+        await newsCollection.updateOne({
+            _id: new ObjectId(request.params.id).valueOf()
+        }, {
+            $set: {
+                title: request.body.title,
+                body: request.body.body
+            }
+        });
+
+        rabbitMq.publish('amq.topic', 'news-api.news.updated', Buffer.from(JSON.stringify({
+            id: request.params.id
+        })));
+
+        return await newsCollection.findOne({
+            _id: new ObjectId(request.params.id).valueOf()
+        }) as unknown as News;
+    });
+
+    server.delete<{ Params: { id: string } }>('/:id', async (request) => {
+        await newsCollection.deleteOne({
+            _id: new ObjectId(request.params.id).valueOf()
+        });
+
+        rabbitMq.publish('amq.topic', 'news-api.news.deleted', Buffer.from(JSON.stringify({
+            id: request.params.id
+        })));
+
+        return {};
+    });
+
+    server.listen({ port: 8080 }, (err, address) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
         }
+        console.log(`Server listening at ${address}`);
     });
+};
 
-    return await newsCollection.findOne({
-        _id: new ObjectId(request.params.id).valueOf()
-    }) as unknown as News;
-});
-
-server.delete('/', async () => {
-    await newsCollection.deleteMany({});
-
-    return {};
-});
-
-server.delete<{ Params: { id: string } }>('/:id', async (request) => {
-    await newsCollection.deleteOne({
-        _id: new ObjectId(request.params.id).valueOf()
-    });
-
-    return {};
-});
-
-server.listen({ port: 8080 }, (err, address) => {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    }
-    console.log(`Server listening at ${address}`);
-});
+app();
